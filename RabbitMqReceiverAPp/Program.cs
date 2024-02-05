e0f8c97fb78e4301256d59c17f234215206b17d2
Milad Kardgar <m.kardgar@sabinarya.com>
2024-02-05 10:26:20 +0330

    Initial commit

---
+++ RabbitMqReceiverAPp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqReceiverApp;
+
+internal abstract class RabbitMqReceiverApp
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "172.16.40.47",
+            UserName = "m.kardgar",
+            Password = "14381438Aa",
+            Port = 5672
+        };
+        using var connection = factory.CreateConnection();
+        var queueOtp = "SmsQueue";
+
+        using (var channel = connection.CreateModel())
+        {
+            IDictionary<String, Object> args = new Dictionary<String, Object>();
+            channel.BasicQos(0, 10, false);
+            var consumer = new QueueingBasicConsumer(channel);
+            consumer.m_consumerCancelled = null;
+            consumer.ConsumerTag = null;
+            IDictionary<string, object> consumerArgs = new Dictionary<string, object>();
+            channel.BasicConsume(queueOtp, false, "", args, consumer);
+            Console.WriteLine(" [*] Waiting for messages. " +
+                              "To exit press CTRL+C");
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+                channel.BasicAck(ea.DeliveryTag, false);
+            }
+        }
+    }
+}
---
+++ RabbitMqSenderApp/RabbitMqSenderApp.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMqSenderApp;
+
+internal abstract class RabbitMqSenderApp
+{
+    public static void Main()
+    {
+        ConnectionFactory factory = new()
+        {
+            Uri = new Uri("amqp://m.kardgar:14381438Aa@172.16.40.47:5672"),
+            ClientProvidedName = "Rabbit Sender App"
+        };
+        IConnection cnn = factory.CreateConnection();
+        IModel channel = cnn.CreateModel();
+        var exchangeName = "SMS";
+        var routingKey = "sms-routing-key";
+        var queueOtp = "SmsQueue";
+        IDictionary <String , Object> args = new Dictionary<String,Object>() ;
+        args.Add("x-max-priority", 24);
+        channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+        channel.QueueDeclare(queueOtp, true, false, false, args);
+        channel.QueueBind(queueOtp, exchangeName, routingKey, null);
+
+        var random = new Random();
+        for (var i = 0; i < 300; i++)
+        {
+            var pr = random.Next(1, 6);
+            var message = "";
+            switch (pr)
+            {
+                case 5:
+                    message = "Otp: " + random.Next(1020, 9090);
+                    break;
+                case 4:
+                    message = "Transaction Amount: " + random.Next(102044, 909099);
+                    break;
+                case 3:
+                    message = "Notice";
+                    break;
+                case 2:
+                    message = "Reverse";
+                    break;
+                case 1:
+                    message = "Public message";
+                    break;
+            }
+            var data = new
+            {
+                To="09124211477,09356070351",
+                Message=message,
+                Priority=pr,
+                DateTime = DateTime.Now
+            };
+            var messageBodyBytes = Encoding.UTF8.GetBytes(System.Text.Json.JsonSerializer.Serialize(data));
+            var properties = channel.CreateBasicProperties();
+            properties.Priority = Convert.ToByte(pr);
+            channel.BasicPublish(exchangeName, routingKey, properties, messageBodyBytes);
+            Thread.Sleep(1000);
+        }
+    }
+}
